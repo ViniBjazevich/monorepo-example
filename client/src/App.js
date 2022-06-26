f52cedbc5ed322bf13323de470fa1f2cf17c51ae
Vini Bjazevich <vinibjazevich@Vinis-iMac.local>
2022-06-26 10:26:43 -0500

    Making server message display after 2 seconds

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,25 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     axios
       .get("http://localhost:8080")
-      .then(({ data }) => setMessage(data))
+      .then(({ data }) => {
+        setTimeout(() => {
+          setMessage(data);
+        }, 2000);
+      })
       .catch((e) => console.log(e));
   }, []);
 
-  return <div className="App">
-    <h1>Monorepo Example</h1>
-    <div>{message}</div>
-  </div>;
+  return (
+    <div className="App">
+      <h1>Monorepo Example</h1>
+      <div>{message ?? "No message found"}</div>
+    </div>
+  );
 }
 
 export default App;
